9a4226877a72abe8819a025b402d5ff2539619dd
allanpedernal <allanpedernal@gmail.com>
2026-02-04 10:49:19 +0800

    Add event listeners for user login/logout and define gate for viewing audit logs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use App\Listeners\LogSuccessfulLogin;
+use App\Listeners\LogSuccessfulLogout;
 use Carbon\CarbonImmutable;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
 
@@ -24,6 +30,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        $this->registerEventListeners();
+        $this->registerGates();
     }
 
     protected function configureDefaults(): void
@@ -43,5 +51,18 @@ class AppServiceProvider extends ServiceProvider
                 ->uncompromised()
             : null
         );
+    }
+
+    protected function registerEventListeners(): void
+    {
+        Event::listen(Login::class, LogSuccessfulLogin::class);
+        Event::listen(Logout::class, LogSuccessfulLogout::class);
+    }
+
+    protected function registerGates(): void
+    {
+        Gate::define('viewAuditLogs', function ($user) {
+            return $user->isSuperAdmin();
+        });
     }
 }
